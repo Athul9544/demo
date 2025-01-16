0945a5fc8120d0d6f38f1e317f8e396c7bd22b3d
Athul9544 <athulpreman01@gmail.com>
2025-01-16 14:41:43 +0530

    Add files via upload

---
+++ mail.py
@@ -0,0 +1,19 @@
+import pickle
+import streamlit as st
+from os import path
+import numpy as np
+
+st.title("flower classification app")
+
+filename="lr.pk"
+with open(path.join("model",filename),'rb') as f:
+    lr=pickle.load(f)
+    
+sl=st.number_input("insert a sepel length")
+sw=st.number_input("insert a sepel width")
+pl=st.number_input("insert a petal lemgth")
+pw=st.number_input("insert a petal length")
+    
+if st.button("predict"):
+    pred=lr.predict(np.array([[sl,sw,pl,pw]]))
+    st.write("the flower is:",pred[0])
